a773472014b0d51e408193d14c6ab045ccd0a87a
Al-Imran <alimran21feb@gmail.com>
2022-04-26 23:49:05 +0600

    added steps

---
+++ __server__deploy.js
@@ -0,0 +1,14 @@
+/**
+ * -----------------------
+ * One Time for your computer
+ * -----------------------
+ * 1. Create heroku account
+ * 2. Verify email
+ * 3. Install heroku CLI
+ * 4. Heroku login
+ * ---------------------------
+ * For each project one time
+ * ---------------------------
+ * 1. Heroku create
+ * 2. 
+ * */ 
